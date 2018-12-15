1faab24c635e248855bf6f9ce7b3d53453c3adee
Boki23m <boki23usa@yahoo.com>
2018-12-15 10:06:21 -0800

    js functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,11 @@
-// creating variables to hold the number of wins, losses and setting them all to zero 
-var wins = 0;
-var losses = 0;
-var computerGuess = [];
-var crystalsValue = [];
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-var randomNumber = document.getElementById("random");
-var totalScoreText = document.getElementById("total-score");
-
 $(document).ready(function () {
+
+    // creating variables to hold the number of wins, losses and setting them all to zero 
+    var wins = 0;
+    var losses = 0;
+    var score = 0;
+    var computerGuess = 0;
+    var images = ["assets/images/orange.png", "assets/images/red.png", "assets/images/blue.png", "assets/images/purple.png"]
 
     // Call the function to initiate the Game
     playGame();
@@ -21,11 +16,30 @@ $(document).ready(function () {
         console.log(computerGuess); // for test only
         $("#random").text(computerGuess);
 
-        for (var i = 0; i < 4; i++) {
-            var n = [Math.floor(Math.random() * 12) +1];
-            crystalsValue.push(n);
+        for (var i = 0; i < images.length; i++) {
+            var crystal = $("<img>");
+            crystal.addClass("crystal");
+            crystal.attr("src", images[i]);
+            crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+            crystal.attr("height", "80px");
+            $(".crystal-images").append(crystal);
         }
-        console.log(crystalsValue);
-    }
 
+        function getCrystalValue() {
+
+            score += parseInt($(this).val());
+            console.log("UserTotal=" + score);
+            $("#total-score").text("Your total score is: " + score);
+            $("#player-score").text(score);
+
+            if (score === computerGuess) {
+                winner()
+            }
+
+            else if (score > computerGuess) {
+                loser()
+            }
+        }
+        $(document).on("click", ".crystal", getCrystalValue);
+    }
 });
